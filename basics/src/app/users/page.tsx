d779cfe7d2e9c0d0b9d698bfd63ac3af5cbb9b24
FarzeenZainab <farzeenzaidi51@gmail.com>
2023-12-21 10:36:03 +0500

    added mothods to delete, update and modify data

--- basics/src/app/users/page.tsx
+++ basics/src/app/users/page.tsx
@@ -54,11 +54,64 @@ const Users = () => {
       });
   };
 
+  const addUser = () => {
+    const newUser = {
+      id: 2,
+      name: "Farzeen Zainab",
+    };
+
+    const originalUsers = [...users]; // keeps track of original users' list
+
+    setUsers([...users, newUser]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users/", newUser)
+      .then(({ data: savedUsers }) => setUsers([savedUsers, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: Users) => {
+    const updatedUser = { ...user, name: "Farzeen" };
+    const originalUsers = users;
+
+    setUsers(
+      users.map((user) => {
+        return updatedUser.id === user.id ? updatedUser : user;
+      })
+    );
+
+    // difference between put and patch method is that in http we use the put method to replace an object and patch method updates one or more properties of an object
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-destructive p-4">{error}</p>}
       {loading && <p className="text-primary p-4">{loading}</p>}
-      <ul className="max-w-[300px] p-6">
+
+      <Button
+        size="sm"
+        onClick={() => {
+          addUser();
+        }}
+        className="m-6"
+      >
+        Add User
+      </Button>
+
+      <ul className="max-w-[600px] p-6">
         {users.map((user) => {
           return (
             <>
@@ -67,15 +120,26 @@ const Users = () => {
                 className="flex justify-between items-center mb-6"
               >
                 {user.name}{" "}
-                <Button
-                  variant="destructive"
-                  size="sm"
-                  onClick={() => {
-                    deleteUser(user);
-                  }}
-                >
-                  Delete User
-                </Button>
+                <div className="flex items-center justify-center gap-2">
+                  <Button
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => {
+                      deleteUser(user);
+                    }}
+                  >
+                    Delete User
+                  </Button>
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => {
+                      updateUser(user);
+                    }}
+                  >
+                    Update User
+                  </Button>
+                </div>
               </li>
             </>
           );
